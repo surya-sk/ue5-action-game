3e8260afe9853df1c286b32f866c77bd4ad6dc04
surya-sk <surya.sk05@outlook.com>
2022-11-25 18:18:33 +0530

    Replace project files

---
+++ Source/UEActionGame/UEActionGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEActionGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEACTIONGAME_API AUEActionGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
