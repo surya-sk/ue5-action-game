f6cce93814de0df152b9ee8510092930e7daafbb
surya-sk <surya.sk05@outlook.com>
2022-11-28 19:10:21 +0530

    Derive weapon class from item

---
+++ Source/UEActionGame/Public/Items/Weapons/Weapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Item.h"
+#include "Weapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEACTIONGAME_API AWeapon : public AItem
+{
+	GENERATED_BODY()
+	
+};
