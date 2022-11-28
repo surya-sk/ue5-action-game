12a9351352e7a14031290118dcf5597942611391
surya-sk <surya.sk05@outlook.com>
2022-11-28 19:28:28 +0530

    Override overlap functions

--- Source/UEActionGame/Private/Items/Weapons/Weapon.cpp
+++ Source/UEActionGame/Private/Items/Weapons/Weapon.cpp
@@ -3,3 +3,14 @@
 
 #include "Items/Weapons/Weapon.h"
 
+void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, 
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+}
+
+void AWeapon::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, 
+	int32 OtherBodyIndex)
+{
+	Super::OnSphereEndOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+}
