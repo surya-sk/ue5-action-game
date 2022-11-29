f94c86b993336c4da92fb382825a1685735b1ea0
surya-sk <surya.sk05@outlook.com>
2022-11-29 13:13:30 +0530

    Retarget animations and attach weapon to mesh

--- Source/UEActionGame/Private/Items/Weapons/Weapon.cpp
+++ Source/UEActionGame/Private/Items/Weapons/Weapon.cpp
@@ -2,11 +2,19 @@
 
 
 #include "Items/Weapons/Weapon.h"
+#include "Characters/MainCharacter.h"
 
 void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, 
 	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	AMainCharacter* MainCharacter = Cast<AMainCharacter>(OtherActor);
+	if (MainCharacter)
+	{
+		FAttachmentTransformRules TransformRules(EAttachmentRule::SnapToTarget, true);
+		ItemMesh->AttachToComponent(MainCharacter->GetMesh(), TransformRules, FName("LeftHandSocket"));
+	}
 }
 
 void AWeapon::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, 
