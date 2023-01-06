4eec2c197dd56ed7719a9877c85f673cf09e85aa
surya-sk <surya.sk05@outlook.com>
2023-01-05 17:38:16 -0800

    Disable tick for Quest

--- Source/UEActionGame/Private/Progression/Quest.cpp
+++ Source/UEActionGame/Private/Progression/Quest.cpp
@@ -8,7 +8,7 @@
 AQuest::AQuest()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 }
 
 // Called when the game starts or when spawned
