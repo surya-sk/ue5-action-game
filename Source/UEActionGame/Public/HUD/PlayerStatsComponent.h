dbd546deb34d4ca6255be8cf28ea30eaba617acc
surya-sk <surya.sk05@outlook.com>
2023-01-08 16:12:45 -0800

    Create widget component class

---
+++ Source/UEActionGame/Public/HUD/PlayerStatsComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "PlayerStatsComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEACTIONGAME_API UPlayerStatsComponent : public UWidgetComponent
+{
+	GENERATED_BODY()
+	
+};
