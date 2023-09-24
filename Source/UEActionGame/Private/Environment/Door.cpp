040335a4de2c6201a29532bcd3623dc342b496c7
surya-sk <surya.sk05@outlook.com>
2023-09-24 16:33:30 -0400

    Create an interactable door

---
+++ Source/UEActionGame/Private/Environment/Door.cpp
@@ -0,0 +1,71 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Environment/Door.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+ADoor::ADoor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	bIsClosed = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = Mesh;
+
+	OpenTrigger = CreateDefaultSubobject<UBoxComponent>(TEXT("OpenTrigger"));
+	OpenTrigger->SetupAttachment(RootComponent);
+
+	CloseTrigger = CreateDefaultSubobject<UBoxComponent>(TEXT("CloseTrigger"));
+	CloseTrigger->SetupAttachment(RootComponent);
+
+	OpenRotationValue = 90.f;
+	CloseRotationValue = 0.f;
+	CurrentRotation = CloseRotationValue;
+	TargetRotation = CloseRotationValue;
+}
+
+// Called when the game starts or when spawned
+void ADoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OpenTrigger->OnComponentBeginOverlap.AddDynamic(this, &ADoor::OnOpenTriggerOverlapBegin);
+	CloseTrigger->OnComponentBeginOverlap.AddDynamic(this, &ADoor::OnCloseTriggerOverlapBegin);
+}
+
+void ADoor::OnOpenTriggerOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->ActorHasTag(FName("PlayerCharacter")))
+	{
+		TargetRotation = OpenRotationValue;
+	}
+}
+
+void ADoor::OnCloseTriggerOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->ActorHasTag(FName("PlayerCharacter")))
+	{
+		TargetRotation = CloseRotationValue;
+	}
+}
+
+void ADoor::RotateDoor(float DeltaTime)
+{
+	if (CurrentRotation != TargetRotation)
+	{
+		CurrentRotation = FMath::FInterpTo(CurrentRotation, TargetRotation, DeltaTime, 2.0f);
+		Mesh->SetRelativeRotation(FRotator(0.f, CurrentRotation, 0.f));
+	}
+}
+
+// Called every frame
+void ADoor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	RotateDoor(DeltaTime);
+}
+
---
+++ Source/UEActionGame/Public/Environment/Door.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Door.generated.h"
+
+UCLASS()
+class UEACTIONGAME_API ADoor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADoor();
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class UBoxComponent* CloseTrigger;
+
+	UPROPERTY(VisibleAnywhere,  BlueprintReadWrite)
+	class UBoxComponent* OpenTrigger;
+
+	UPROPERTY(EditAnywhere)
+	float OpenRotationValue;
+
+	UPROPERTY(EditAnywhere)
+	float CloseRotationValue;
+
+	UFUNCTION()
+	void OnOpenTriggerOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void  OnCloseTriggerOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:
+	bool bIsClosed;
+	float CurrentRotation;
+	float TargetRotation;
+
+	void RotateDoor(float DeltaTime);
+
+};
