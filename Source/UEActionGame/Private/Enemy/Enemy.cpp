90e33aff0d9756b32cd8dca111bef19bb98e66fa
surya-sk <surya.sk05@outlook.com>
2023-10-19 23:34:24 -0400

    Polish enemy AI

--- Source/UEActionGame/Private/Enemy/Enemy.cpp
+++ Source/UEActionGame/Private/Enemy/Enemy.cpp
@@ -375,7 +375,6 @@ void AEnemy::PawnSeen(APawn* SeenPawn)
 		Player && !Player->IsFollowing();
 	if (bShouldChase)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("%d"), Player->IsFollowing());
 		CombatTarget = SeenPawn;
 		GetWorldTimerManager().ClearTimer(PatrolTimer);
 		ChaseTarget();
