c5a6c89abd62087309809fe94c7e5e5c17765b8a
surya-sk <surya.sk05@outlook.com>
2023-11-07 19:52:37 -0500

    Add more dialgue

--- Source/UEActionGame/Public/Dialogue/DialogueManager.h
+++ Source/UEActionGame/Public/Dialogue/DialogueManager.h
@@ -9,8 +9,18 @@
 UENUM(BlueprintType)
 enum class EDialogueKey
 {
+	/* Test*/
 	EDK_Echo_Hello UMETA(DisplayName = "Echo Hello"),
-	EDK_NPC_Hello UMETA(DisplayName = "NPC hello")
+	EDK_NPC_Hello UMETA(DisplayName = "NPC hello"),
+
+	/*Alex and Sarah's conversation*/
+	EDK_Alex_Hello UMETA(DisplayName = "Alex: How can I help you?"),
+	EDK_Sarah_HaveYouSeenAnythingWeird UMETA(DisplayName = "Sarah: Have you seen anything weird around here? I've woken up to some strange objects around me."),
+	EDK_Alex_NotReally UMETA(DisplayName = "Alex: Not really, but guards from the regime often come by to scour for witches. Could've been them."),
+	EDK_Sarah_ThatWouldExplainThings UMETA(DisplayName =  "Sarah: Oh that would explain the symbols of the witch hunters."),
+	EDK_Alex_ProbablyMarkedAsWitch UMETA(DisplayName = "Alex: Ah, that probably means you've been marked as a witch."),
+	EDK_Sarah_IronicCauseImKillingThem UMETA(DisplayName = "Sarah: Ha, ironic considering I'm taking down these bastards for killing innocent women they think are witches."),
+	EDK_Alex_BeCareful UMETA(DisplayName = "Alex: Well, I would be very careful if I were you. I heard about a temple near the lake into the woods where one of the leaders operates. Powerful and ruthless man from what I've heard. Has to be stopped if you ask me. Has been killing a lot of folks around this town. ")
 };
 
 USTRUCT(BlueprintType)
