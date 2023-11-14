50895ca890e2035d7e7eb73927b9a41794d70793
surya-sk <surya.sk05@outlook.com>
2023-11-13 21:52:20 -0500

    Create PresentDayCharacter class

---
+++ Source/UEActionGame/Public/Characters/PresentDayCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/BaseCharacter.h"
+#include "PresentDayCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEACTIONGAME_API APresentDayCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
