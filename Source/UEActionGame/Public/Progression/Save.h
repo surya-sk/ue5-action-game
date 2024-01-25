4c05c9902af73ec4faace020e66c3e7e4b52adb9
surya-sk <surya.sk05@outlook.com>
2024-01-25 18:42:24 +0530

    Create save game class

---
+++ Source/UEActionGame/Public/Progression/Save.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "Save.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEACTIONGAME_API USave : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
